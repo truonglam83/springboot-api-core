dead295a790b5772f54a4065d46c48568ab8b3fc
truonglam83 <truonglam83.dev@gmail.com>
2026-03-14 21:09:33 +0700

    - add config

--- src/main/java/com/api/coreapi/sys/sample/SampleController.java
+++ src/main/java/com/api/coreapi/sys/sample/SampleController.java
@@ -4,6 +4,7 @@ import com.api.coreapi.sys.sample.dto.SampleRequestDto;
 import com.api.coreapi.sys.sample.dto.SampleResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class SampleController {
     private final SampleService sampleService;
 
     @PostMapping("/getListSample")
-    public SampleResponseDto getListSample(SampleRequestDto request) {
+    public SampleResponseDto getListSample(@RequestBody SampleRequestDto request) {
         return sampleService.getListSample(request);
     }
 }
